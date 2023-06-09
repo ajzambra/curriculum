a46341eaa0d27655f91670acfa7395e26e6e1897
Ajzambra <ajzambra@espol.edu.ec>
2023-06-09 12:49:04 -0500

    Subida Final

---
+++ stylesheets/script.js
@@ -0,0 +1,11 @@
+function enviarFormulario(event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+    
+    // Simulación de envío del formulario
+    // Aquí podrías realizar una solicitud AJAX para enviar los datos del formulario al servidor
+    
+    // Mostrar el mensaje de éxito
+    document.getElementById("success-message").classList.remove("hidden");
+  }
+  
+  
